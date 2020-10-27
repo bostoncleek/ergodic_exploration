61ff0b8d91f38d49c79840cb2b3356be92c41039
bostoncleek <bocl8874@colorado.edu>
2020-10-27 18:25:07 -0500

    grid index conversions and data access functionality added

--- include/ergodic_exploration/grid.hpp
+++ include/ergodic_exploration/grid.hpp
@@ -10,20 +10,37 @@
 #include <iosfwd>
 #include <cmath>
 #include <vector>
-#include <memory>
+// #include <memory>
+
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/OccupancyGrid.h>
 
 namespace ergodic_exploration
 {
+typedef std::vector<int8_t> gridData;
+
 /**
  * @brief Length of a grid axis
  * @param lower - lower limit
  * @param upper - upper limit
  * @param resolution - resolution of grid
- * @return lenght
+ * @return length
  */
-unsigned int axisLength(double lower, double upper, double resolution)
+constexpr unsigned int axisLength(double lower, double upper, double resolution)
 {
   return static_cast<unsigned int>(std::round((upper - lower) / resolution));
+}
+
+/**
+ * @brief Upper axis limit
+ * @param lower - position of lower axis limit
+ * @param resolution - grid resolution
+ * @param size - length of axis
+ * @return axis upper limit
+ */
+constexpr double axisUpper(double lower, double resolution, unsigned int size)
+{
+  return static_cast<double>(resolution * size) + lower;
 }
 
 /** @brief Constructs an 2D grid */
@@ -40,13 +57,34 @@ public:
    * @param grid_ptr - shared pointer to current grid data
    */
   GridMap(double xmin, double xmax, double ymin, double ymax, double resolution,
-          const std::shared_ptr<std::vector<int8_t>>& grid_ptr);
+          const gridData& grid_data);
+
+  /**
+   * @brief Constructor
+   * @param grid_msg - ROS occupancy grid message
+   */
+  GridMap(const nav_msgs::OccupancyGrid::ConstPtr& grid_msg);
 
   /**
    * @brief Get the grid data
    * @return grid data
    */
-  const std::shared_ptr<std::vector<int8_t>>& getGridData() const;
+  const gridData& getGridData() const;
+
+  /**
+   * @brief Test if coordinates are within the grid
+   * @param i - row in the grid (y-coordinate)
+   * @param j - column in the grid (x-coordinate)
+   * @return true if coordinates are within the grid
+   */
+  bool gridBounds(unsigned int i, unsigned int j) const;
+
+  /**
+   * @brief Test if coordinates are within the grid
+   * @param idx - row major index
+   * @return true if index is within the grid
+   */
+  bool gridBounds(unsigned int idx) const;
 
   /**
    * @brief Converts grid indices to row major order index
@@ -58,17 +96,69 @@ public:
 
   /**
    * @brief Convert row major index to row and column indices
-   * @param idx - row major column index
+   * @param idx - row major index
    * @return row and column indices
    */
   std::vector<unsigned int> rowMajor2Grid(unsigned int idx) const;
 
-private:
-  double xmin_, xmax_, ymin_, ymax_, resolution_;
-  unsigned int xsize_, ysize_;
+  /**
+   * @brief Convert grid coordinates to world position
+   * @param i - row in the grid (y-coordinate)
+   * @param j - column in the grid (x-coordinate)
+   * @return the x and y coordinates in the world
+   */
+  std::vector<double> grid2World(unsigned int i, unsigned int j) const;
 
-protected:
-  std::shared_ptr<std::vector<int8_t>> grid_ptr_;
+  /**
+   * @brief Convert row major index to world position
+   * @param idx - row major index
+   * @return the x and y coordinates in the world
+   */
+  std::vector<double> grid2World(unsigned int idx) const;
+
+  /**
+   * @brief Convert world coordinates to row and column index in grid
+   * @param x - position in world (jth column)
+   * @param y - position in world (ith row)
+   * @return ith row and jth column in grid
+   */
+  std::vector<unsigned int> world2Grid(double x, double y) const;
+
+  /**
+   * @brief Convert world coordinates to row major index
+   * @param x - position in world (jth column)
+   * @param y - position in world (ith row)
+   * @return row major index
+   */
+  unsigned int world2RowMajor(double x, double y) const;
+
+  /**
+   * @brief Get the value of the cell at a specified position
+   * @param x - position in world (jth column)
+   * @param y - position in world (ith row)
+   * @return value of the cell
+   */
+  int8_t getCell(double x, double y) const;
+
+  /**
+   * @brief Get the value of the cell at a specified index
+   * @param i - row in the grid (y-coordinate)
+   * @param j - column in the grid (x-coordinate)
+   * @return value of the cell
+   */
+  int8_t getCell(unsigned int i, unsigned int j) const;
+
+  /**
+   * @brief Get the value of the cell at a specified index
+   * @param idx - row major index
+   * @return value of the cell
+   */
+  int8_t getCell(unsigned int idx) const;
+
+private:
+  unsigned int xsize_, ysize_;
+  double resolution_, xmin_, ymin_, xmax_, ymax_;
+  gridData grid_data_;
 };
 
 }  // namespace ergodic_exploration
--- src/ergodic_exploration/grid.cpp
+++ src/ergodic_exploration/grid.cpp
@@ -6,27 +6,52 @@
  */
 
 #include <iostream>
+#include <stdexcept>
 
 #include <ergodic_exploration/grid.hpp>
 
 namespace ergodic_exploration
 {
 GridMap::GridMap(double xmin, double xmax, double ymin, double ymax, double resolution,
-                 const std::shared_ptr<std::vector<int8_t>>& grid_ptr)
-  : xmin_(xmin)
-  , xmax_(xmax)
-  , ymin_(ymin)
-  , ymax_(ymax)
-  , resolution_(resolution)
-  , xsize_(axisLength(xmin, xmax, resolution))
+                 const gridData& grid_data)
+  : xsize_(axisLength(xmin, xmax, resolution))
   , ysize_(axisLength(ymin, ymax, resolution))
-  , grid_ptr_(grid_ptr)
+  , resolution_(resolution)
+  , xmin_(xmin)
+  , ymin_(ymin)
+  , xmax_(xmax)
+  , ymax_(ymax)
+  , grid_data_(grid_data)
 {
 }
 
-const std::shared_ptr<std::vector<int8_t>>& GridMap::getGridData() const
+GridMap::GridMap(const nav_msgs::OccupancyGrid::ConstPtr& grid_msg)
+  : xsize_(grid_msg->info.width)
+  , ysize_(grid_msg->info.height)
+  , resolution_(grid_msg->info.resolution)
+  , xmin_(grid_msg->info.origin.position.x)
+  , ymin_(grid_msg->info.origin.position.y)
+  , xmax_(axisUpper(xmin_, resolution_, xsize_))
+  , ymax_(axisUpper(ymin_, resolution_, ysize_))
+  , grid_data_(grid_msg->data)
 {
-  return grid_ptr_;
+}
+
+const gridData& GridMap::getGridData() const
+{
+  return grid_data_;
+}
+
+bool GridMap::gridBounds(unsigned int i, unsigned int j) const
+{
+  // unsigned int is always greater than 0
+  return ((i <= ysize_ - 1) and (j <= xsize_ - 1)) ? true : false;
+}
+
+bool GridMap::gridBounds(unsigned int idx) const
+{
+  const auto indices = rowMajor2Grid(idx);
+  return ((indices.at(0) <= ysize_ - 1) and (indices.at(1) <= xsize_ - 1)) ? true : false;
 }
 
 unsigned int GridMap::grid2RowMajor(unsigned int i, unsigned int j) const
@@ -42,4 +67,66 @@ std::vector<unsigned int> GridMap::rowMajor2Grid(unsigned int idx) const
   return { i, j };
 }
 
+std::vector<double> GridMap::grid2World(unsigned int i, unsigned int j) const
+{
+  const auto x = static_cast<double>(j * resolution_) + resolution_ / 2.0 + xmin_;
+  const auto y = static_cast<double>(i * resolution_) + resolution_ / 2.0 + ymin_;
+  return { x, y };
+}
+
+std::vector<double> GridMap::grid2World(unsigned int idx) const
+{
+  const auto indices = rowMajor2Grid(idx);
+  const auto x =
+      static_cast<double>(indices.at(1) * resolution_) + resolution_ / 2.0 + xmin_;
+  const auto y =
+      static_cast<double>(indices.at(0) * resolution_) + resolution_ / 2.0 + ymin_;
+  return { x, y };
+}
+
+std::vector<unsigned int> GridMap::world2Grid(double x, double y) const
+{
+  unsigned int j = static_cast<unsigned int>(std::floor((x - xmin_) / resolution_));
+  unsigned int i = static_cast<unsigned int>(std::floor((y - ymin_) / resolution_));
+
+  if (j == xsize_)
+  {
+    j--;
+  }
+
+  if (i == ysize_)
+  {
+    i--;
+  }
+
+  return { i, j };
+}
+
+unsigned int GridMap::world2RowMajor(double x, double y) const
+{
+  const auto indices = world2Grid(x, y);
+  return grid2RowMajor(indices.at(0), indices.at(1));
+}
+
+int8_t GridMap::getCell(double x, double y) const
+{
+  return getCell(world2RowMajor(x, y));
+}
+
+int8_t GridMap::getCell(unsigned int i, unsigned int j) const
+{
+  return getCell(grid2RowMajor(i, j));
+}
+
+int8_t GridMap::getCell(unsigned int idx) const
+{
+  if (!gridBounds(idx))
+  {
+    throw std::invalid_argument("Grid index out of range");
+  }
+  return grid_data_.at(idx);
+}
+
 }  // namespace ergodic_exploration
+
+//
