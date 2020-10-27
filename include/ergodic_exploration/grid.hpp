7aa68e95cf55514170fe424005c8a4369014a09b
bostoncleek <bocl8874@colorado.edu>
2020-10-26 21:36:36 -0500

    conversions to row major index and return pointer to grid data functionality added

--- include/ergodic_exploration/grid.hpp
+++ include/ergodic_exploration/grid.hpp
@@ -10,6 +10,7 @@
 #include <iosfwd>
 #include <cmath>
 #include <vector>
+#include <memory>
 
 namespace ergodic_exploration
 {
@@ -25,14 +26,6 @@ unsigned int axisLength(double lower, double upper, double resolution)
   return static_cast<unsigned int>(std::round((upper - lower) / resolution));
 }
 
-/** @brief Grid cell */
-struct Cell
-{
-  Cell(): i(-1), j(-1), marked(false) {}
-  unsigned int i, j;
-  bool marked;
-};
-
 /** @brief Constructs an 2D grid */
 class GridMap
 {
@@ -44,25 +37,38 @@ public:
    * @param ymin - y low bound of grid
    * @param ymax - y upper bound of grid
    * @param resolution - grid resolution
+   * @param grid_ptr - shared pointer to current grid data
    */
-  GridMap(double xmin, double xmax, double ymin, double ymax, double resolution);
+  GridMap(double xmin, double xmax, double ymin, double ymax, double resolution,
+          const std::shared_ptr<std::vector<int8_t>>& grid_ptr);
 
-private:
+  /**
+   * @brief Get the grid data
+   * @return grid data
+   */
+  const std::shared_ptr<std::vector<int8_t>>& getGridData() const;
+
   /**
    * @brief Converts grid indices to row major order index
    * @param i - row in the grid (y-coordinate)
    * @param j - column in the grid (x-coordinate)
    * @return grid index in row major order
    */
-  unsigned int grid2RowMajor(int i, int j) const;
+  unsigned int grid2RowMajor(unsigned int i, unsigned int j) const;
+
+  /**
+   * @brief Convert row major index to row and column indices
+   * @param idx - row major column index
+   * @return row and column indices
+   */
+  std::vector<unsigned int> rowMajor2Grid(unsigned int idx) const;
 
 private:
   double xmin_, xmax_, ymin_, ymax_, resolution_;
   unsigned int xsize_, ysize_;
 
 protected:
-  std::vector<Cell> grid_;
-
+  std::shared_ptr<std::vector<int8_t>> grid_ptr_;
 };
 
 }  // namespace ergodic_exploration
--- src/ergodic_exploration/grid.cpp
+++ src/ergodic_exploration/grid.cpp
@@ -11,7 +11,8 @@
 
 namespace ergodic_exploration
 {
-GridMap::GridMap(double xmin, double xmax, double ymin, double ymax, double resolution)
+GridMap::GridMap(double xmin, double xmax, double ymin, double ymax, double resolution,
+                 const std::shared_ptr<std::vector<int8_t>>& grid_ptr)
   : xmin_(xmin)
   , xmax_(xmax)
   , ymin_(ymin)
@@ -19,14 +20,26 @@ GridMap::GridMap(double xmin, double xmax, double ymin, double ymax, double reso
   , resolution_(resolution)
   , xsize_(axisLength(xmin, xmax, resolution))
   , ysize_(axisLength(ymin, ymax, resolution))
-  , grid_(xsize_ * ysize_)
+  , grid_ptr_(grid_ptr)
 {
 }
 
-unsigned int GridMap::grid2RowMajor(int i, int j) const
+const std::shared_ptr<std::vector<int8_t>>& GridMap::getGridData() const
+{
+  return grid_ptr_;
+}
+
+unsigned int GridMap::grid2RowMajor(unsigned int i, unsigned int j) const
 {
   // ith_row * Ncols + jth_col
   return i * xsize_ + j;
+}
+
+std::vector<unsigned int> GridMap::rowMajor2Grid(unsigned int idx) const
+{
+  const auto i = static_cast<unsigned int>(idx / xsize_);
+  const auto j = idx - i * xsize_;
+  return { i, j };
 }
 
 }  // namespace ergodic_exploration
