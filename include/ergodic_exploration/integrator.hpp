c1102f579c84c3fe13908e2f16d738f23dbd07e4
bostoncleek <bocl8874@colorado.edu>
2020-10-30 19:31:00 -0500

    robot model is template parameter

--- include/ergodic_exploration/integrator.hpp
+++ include/ergodic_exploration/integrator.hpp
@@ -5,7 +5,6 @@
  * @brief Numerical integration methods
  */
 
-#include <functional>
 #include <armadillo>
 
 #include <ergodic_exploration/collision.hpp>
@@ -19,10 +18,7 @@ using arma::vec;
  * @brief Function representing a dynamic system
  * @details xdot = f(x,u)
  */
-typedef std::function<vec(const vec&, const vec&)> DynaFunc;
-
-// template<class T>
-// using CoState = std::function<void(T)>;
+// typedef std::function<vec(const vec&, const vec&)> DynaFunc;
 
 /** @brief 4th order Runge-Kutta integration */
 class RungeKutta
@@ -42,7 +38,8 @@ public:
    * @param horizon - length of trajectory in time
    * @return new state (column vector)
    */
-  mat solve(const DynaFunc& func, const vec& x0, const mat& ut, double horizon);
+  template <class T>
+  mat solve(const T& model, const vec& x0, const mat& ut, double horizon);
 
   /**
    * @brief Performs one step of RK4
@@ -51,7 +48,8 @@ public:
    * @param u - control (column vector)
    * @return new state (column vector)
    */
-  vec step(const DynaFunc& func, const vec& x, const vec& u);
+  template <class T>
+  vec step(const T& model, const vec& x, const vec& u);
 
   /**
    * @brief Performs one step of RK4
@@ -65,17 +63,46 @@ public:
    * detector composes the boundary derivatives (db/dx), collision derivatives (dc/dx),
    * and range finder collisions (dr/dx).
    */
-  template <class T>
-  vec step(const T& model, const Collision& collision, const mat& xt, const mat& ut, const vec& edx);
+  // template <class T>
+  // vec step(const T& model, const Collision& collision, const mat& xt, const mat& ut, const vec& edx);
 
 private:
   double dt_;
 };
 
-template <class T>
-vec RungeKutta::step(const T& model, const Collision& collision, const mat& xt,
-                     const mat& ut, const vec& edx)
+RungeKutta::RungeKutta(double dt) : dt_(dt)
 {
 }
+
+template <class T>
+mat RungeKutta::solve(const T& model, const vec& x0, const mat& ut, double horizon)
+{
+  vec x = x0;
+  const auto steps = static_cast<unsigned int>(horizon / dt_);
+  mat xt(x0.n_rows, steps);
+
+  for (unsigned int i = 0; i < steps; i++)
+  {
+    x = step(model, x, ut.col(i));
+    xt.col(i) = x;
+  }
+  return xt;
+}
+
+template <class T>
+vec RungeKutta::step(const T& model, const vec& x, const vec& u)
+{
+  const vec k1 = model(x, u);
+  const vec k2 = model(x + dt_ * (0.5 * k1), u);
+  const vec k3 = model(x + dt_ * (0.5 * k2), u);
+  const vec k4 = model(x + dt_ * k3, u);
+  return x + dt_ / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
+}
+
+// template <class T>
+// vec RungeKutta::step(const T& model, const Collision& collision, const mat& xt,
+//                      const mat& ut, const vec& edx)
+// {
+// }
 
 }  // namespace ergodic_exploration
