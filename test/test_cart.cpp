e62577fbbd3c041e28724a60cbd214dbf591643a
bostoncleek <bocl8874@colorado.edu>
2020-10-30 19:36:20 -0500

    test cart kinematics

---
+++ test/test_cart.cpp
@@ -0,0 +1,94 @@
+/**
+ * @file test_cart.cpp
+ * @author Boston Cleek
+ * @date 30 Oct 2020
+ * @brief Test cart model
+ */
+
+#include <gtest/gtest.h>
+#include <ergodic_exploration/cart.hpp>
+
+TEST(CartTest, CartKinematics)
+{
+  const auto wheel_radius = 0.033;
+  const auto wheel_base = 0.08;
+  ergodic_exploration::Cart cart(wheel_radius, wheel_base);
+
+  const arma::vec x = { 1.0, 2.0, 0.707 };
+  const arma::vec u = { 0.1, 0.2 };
+  const arma::vec xdot = cart(x, u);
+
+  ASSERT_NEAR(xdot(0), 0.003763, 1e-6);
+  ASSERT_NEAR(xdot(1), 0.003215, 1e-6);
+  ASSERT_NEAR(xdot(2), 0.020625, 1e-6);
+}
+
+TEST(CartTest, CartJacobianState)
+{
+  const auto wheel_radius = 0.033;
+  const auto wheel_base = 0.08;
+  ergodic_exploration::Cart cart(wheel_radius, wheel_base);
+
+  const arma::vec x = { 1.0, 2.0, 0.707 };
+  const arma::vec u = { 0.1, 0.2 };
+  const arma::mat A = cart.fdx(x, u);
+
+  ASSERT_NEAR(A(0, 2), -0.003215, 1e-6);
+  ASSERT_NEAR(A(1, 2), 0.003763, 1e-6);
+}
+
+TEST(CartTest, CartJacobianControl)
+{
+  const auto wheel_radius = 0.033;
+  const auto wheel_base = 0.08;
+  ergodic_exploration::Cart cart(wheel_radius, wheel_base);
+
+  const arma::vec x = { 1.0, 2.0, 0.707 };
+  const arma::mat B = cart.fdu(x);
+
+  ASSERT_NEAR(B(0, 0), 0.012545, 1e-6);
+  ASSERT_NEAR(B(0, 1), 0.012545, 1e-6);
+
+  ASSERT_NEAR(B(1, 0), 0.010717, 1e-6);
+  ASSERT_NEAR(B(1, 1), 0.010717, 1e-6);
+
+  ASSERT_NEAR(B(2, 0), -0.20625, 1e-6);
+  ASSERT_NEAR(B(2, 1), 0.20625, 1e-6);
+}
+
+TEST(CartTest, SimpleCartKinematics)
+{
+  ergodic_exploration::SimpleCart cart;
+
+  const arma::vec x = { 1.0, 2.0, 0.707 };
+  const arma::vec u = { 0.5, 0.01 };
+  const arma::vec xdot = cart(x, u);
+
+  ASSERT_NEAR(xdot(0), 0.380156, 1e-6);
+  ASSERT_NEAR(xdot(1), 0.324777, 1e-6);
+  ASSERT_NEAR(xdot(2), 0.01, 1e-6);
+}
+
+TEST(CartTest, SimpleCartJacobianState)
+{
+  ergodic_exploration::SimpleCart cart;
+
+  const arma::vec x = { 1.0, 2.0, 0.707 };
+  const arma::vec u = { 0.5, 0.01 };
+  const arma::mat A = cart.fdx(x, u);
+
+  ASSERT_NEAR(A(0, 2), -0.324777, 1e-6);
+  ASSERT_NEAR(A(1, 2), 0.380156, 1e-6);
+}
+
+TEST(CartTest, SimpleCartJacobianControl)
+{
+  ergodic_exploration::SimpleCart cart;
+
+  const arma::vec x = { 1.0, 2.0, 0.707 };
+  const arma::vec u = { 0.5, 0.01 };
+  const arma::mat B = cart.fdu(x);
+
+  ASSERT_NEAR(B(0, 0), 0.760313, 1e-6);
+  ASSERT_NEAR(B(1, 0), 0.649555, 1e-6);
+}
