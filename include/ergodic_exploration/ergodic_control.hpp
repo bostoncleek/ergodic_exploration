eddb3f2becc0c4e27b7ceebe0ceff3a67e671266
bostoncleek <bocl8874@colorado.edu>
2020-11-06 17:16:32 -0600

    base ergodic control loop running... no replay buffer

--- include/ergodic_exploration/ergodic_control.hpp
+++ include/ergodic_exploration/ergodic_control.hpp
@@ -18,7 +18,7 @@ namespace ergodic_exploration
 {
 using arma::span;
 
-// TODO: check base 2 or e
+// TODO: check base 2 or e ???
 /**
  * @brief Entropy of a single grid cell
  * @param prob_occu - probability grid cell is occupied represented as a decimal
@@ -33,6 +33,18 @@ inline double entropy(double p)
   }
 
   return -p * std::log(p) - (1.0 - p) * std::log(1.0 - p);
+}
+
+/**
+ * @brief Time derivatve of the co-state variable
+ * @param rho - co-state variable
+ * @param kldx - ergodic measure derivative
+ * @param fdx - jacobian of the dynamics w.r.t state A = D1(f(x,u))
+ * @return  d/dt[rho]
+ */
+inline vec rhodot(const vec& rho, const vec& kldx, const mat& fdx)
+{
+  return kldx - fdx * rho;
 }
 
 /** @brief Receeding horizon ergodic trajectory optimization */
@@ -63,15 +75,16 @@ private:
   unsigned int num_samples_;
   unsigned int buffer_size_;
   unsigned int steps_;
-  mat R_;
+  mat Rinv_;
   mat Sigmainv_;
   mat ut_;
   mat s_;
   mat edx_;
-  vec pT_;
+  vec rhoT_;
   vec p_;
   vec q_;
   RungeKutta rk4_;
+  CoStateFunc rhodot_;
 };
 
 template <class ModelT>
@@ -84,12 +97,12 @@ ErgodicControl<ModelT>::ErgodicControl(const ModelT& model, double dt, double ho
   , num_samples_(num_samples)
   , buffer_size_(buffer_size)
   , steps_(static_cast<unsigned int>(horizon / dt))
-  , R_(R)
+  , Rinv_(inv(R))
   , Sigmainv_(inv(Sigma))
   , ut_(model.action_space_, steps_, arma::fill::zeros)
   , s_(2, num_samples)
   , edx_(model.state_space_, steps_)
-  , pT_(model.state_space_, arma::fill::zeros)
+  , rhoT_(model.state_space_, arma::fill::zeros)
   , p_(num_samples)
   , q_(num_samples)
   , rk4_(dt)
@@ -99,6 +112,12 @@ ErgodicControl<ModelT>::ErgodicControl(const ModelT& model, double dt, double ho
     throw std::invalid_argument("Need at least two steps in forward simulation. \
                                  Increase the horizon or decrease the time step.");
   }
+
+  arma::arma_rng::set_seed_random();
+  // arma::arma_rng::set_seed(0);
+
+  rhodot_ = std::bind(rhodot, std::placeholders::_1, std::placeholders::_2,
+                      std::placeholders::_3);
 }
 
 template <class ModelT>
@@ -110,22 +129,42 @@ vec ErgodicControl<ModelT>::control(const GridMap& grid, const vec& x)
 
   // Forward simulation
   const mat xt = rk4_.solve(model_, x, ut_, horizon_);
+  // xt.print();
 
   // TODO: add buffer
 
   // Sample (x,y) space
   sample(grid);
+  // p_.print("p:");
 
   // TODO: append xt to buffer and pass in here
   // Time averages trajectory statistics
   trajStat(xt);
+  // q_.print("q:");
 
   // Derivative of the ergodic measure w.r.t the state
   ergMeasDeriv(xt);
+  // edx_.print("edx:");
 
   // Backwards pass
+  const mat rhot = rk4_.solve(rhodot_, model_, rhoT_, xt, ut_, edx_, horizon_);
+  // rhot.print("rho:");
 
   // Update controls
+  for (unsigned int i = 0; i < xt.n_cols; i++)
+  {
+    // index into rho backwards
+    const unsigned int j = (xt.n_cols - 1) - i;
+    ut_.col(i) = -Rinv_ * model_.fdu(xt.col(i)).t() * rhot.col(j);
+
+    // TODO: add control limits as a parameter
+    if (any(ut_.col(i) > 1.0) || any(ut_.col(i) < -1.0))
+    {
+      ut_.col(i) /= accu(ut_.col(i));
+    }
+  }
+
+  // ut_.print("Control:");
 
   return ut_.col(0);
 }
@@ -135,6 +174,7 @@ void ErgodicControl<ModelT>::sample(const GridMap& grid)
 {
   // Uniformly generate sample [0 1]
   s_.randu(2, num_samples_);
+  // s_.print();
 
   // Scale based on grid domain
   s_.row(0) = s_.row(0) * (grid.xmax() - grid.xmin()) + grid.xmin();
@@ -165,9 +205,11 @@ void ErgodicControl<ModelT>::trajStat(const mat& xt)
     {
       const vec diff = s_.col(i) - xt(span(0, 1), span(j, j));
       qval += std::exp(-0.5 * dot(diff.t() * Sigmainv_, diff));
+      // std::cout << std::exp(-0.5 * dot(diff.t() * Sigmainv_, diff)) << std::endl;
     }
 
-    q_(i) = qval;
+    // TODO: add 1e-8 is this is 0 ??
+    q_(i) = qval;  // + 1e-8;
   }
 
   // Normalize the trajectory statistics
@@ -191,6 +233,8 @@ void ErgodicControl<ModelT>::ergMeasDeriv(const mat& xt)
       kldx.rows(0, 1) += (p_(j) / q_(j)) * qval * (Sigmainv_ * diff);
     }
 
+    // TODO: add 1e-8 is this is 0 ??
+    // kldx.rows(0, 1) += 1e-8;
     edx_.col(i) = kldx;
   }
 }
