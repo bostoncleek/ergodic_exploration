7f9231c6aee495449b07c327bd500d567ea8e1a8
bostoncleek <bocl8874@colorado.edu>
2020-11-07 19:30:34 -0600

    control omni using body twist model added

--- include/ergodic_exploration/omni.hpp
+++ include/ergodic_exploration/omni.hpp
@@ -35,14 +35,14 @@ struct Mecanum
     : wheel_radius(wheel_radius)
     , wheel_base_x(wheel_base_x)
     , wheel_base_y(wheel_base_y)
-    , action_space_(4)
-    , state_space_(3)
+    , action_space(4)
+    , state_space(3)
   {
   }
 
   /**
    * @brief Convert wheel velocities to a body frame twist
-   * @param u - control [u0, u1, u2, u3] (column vector)
+   * @param u - control [u0, u1, u2, u3]
    * @return twist in body frame Vb = [vx, vy, w]
    */
   Twist2D wheels2Twist(const vec u) const
@@ -59,9 +59,9 @@ struct Mecanum
 
   /**
    * @brief Kinematic model of 4 mecanum wheel robot
-   * @param x - state [x, y, theta] (column vector)
-   * @param u - control [u0, u1, u2, u3] (column vector)
-   * @return [xdot, ydot, thetadot] = f(x,u) (column vector)
+   * @param x - state [x, y, theta]
+   * @param u - control [u0, u1, u2, u3]
+   * @return [xdot, ydot, thetadot] = f(x,u)
    */
   vec operator()(const vec x, const vec u) const
   {
@@ -79,8 +79,8 @@ struct Mecanum
 
   /**
    * @brief Jacobian of the model with respect to the state
-   * @param x - state [x, y, theta] (column vector)
-   * @param u - control [u0, u1, u2, u3] (column vector)
+   * @param x - state [x, y, theta]
+   * @param u - control [u0, u1, u2, u3]
    * @return A = D1(f(x,u)) of shape (3x3)
    */
   mat fdx(const vec x, const vec u) const
@@ -103,7 +103,7 @@ struct Mecanum
 
   /**
    * @brief Jacobian of the model with respect to the control
-   * @param x - state [x, y, theta] (column vector)
+   * @param x - state [x, y, theta]
    * @return B = D2(f(x,u)) of shape (3x4)
    */
   mat fdu(const vec x) const
@@ -118,11 +118,67 @@ struct Mecanum
     return B;
   }
 
-  double wheel_radius;  // radius of wheel
-  double wheel_base_x;  // distance from chassis center to wheel center along x-axis
-  double wheel_base_y;  // distance from chassis center to wheel center along y-axis
-  unsigned int action_space_;  // control space dimension
-  unsigned int state_space_;   // states space dimension
+  double wheel_radius;        // radius of wheel
+  double wheel_base_x;        // distance from chassis center to wheel center along x-axis
+  double wheel_base_y;        // distance from chassis center to wheel center along y-axis
+  unsigned int action_space;  // control space dimension
+  unsigned int state_space;   // states space dimension
+};
+
+/**
+ * @brief Kinematic model of omni directonal robot
+ * @details The state is [x, y, theta] and controls are the linear and
+ * angular velocities [vx, vy, w] (body twist)
+ */
+struct Omni
+{
+  /** @brief Constructor */
+  Omni() : action_space(3), state_space(3)
+  {
+  }
+
+  /**
+   * @brief Kinematic model of 4 mecanum wheel robot
+   * @param x - state [x, y, theta]
+   * @param u - body twist control [vx, vy, w]
+   * @return [xdot, ydot, thetadot] = f(x,u)
+   */
+  vec operator()(const vec x, const vec u) const
+  {
+    const auto xdot = u(0) * std::cos(x(2)) - u(1) * std::sin(x(2));
+    const auto ydot = u(0) * std::sin(x(2)) + u(1) * std::cos(x(2));
+    return { xdot, ydot, u(2) };
+  }
+
+  /**
+   * @brief Jacobian of the model with respect to the state
+   * @param x - state [x, y, theta]
+   * @param u - body twist control [vx, vy, w]
+   * @return A = D1(f(x,u)) of shape (3x3)
+   */
+  mat fdx(const vec x, const vec u) const
+  {
+    mat A(3, 3, arma::fill::zeros);
+    A(0, 2) = -u(0) * std::sin(x(2)) - u(1) * std::cos(x(2));
+    A(1, 2) = u(0) * std::cos(x(2)) - u(1) * std::sin(x(2));
+    return A;
+  }
+
+  /**
+   * @brief Jacobian of the model with respect to the control
+   * @param x - state [x, y, theta]
+   * @return B = D2(f(x,u)) of shape (3x3)
+   */
+  mat fdu(const vec x) const
+  {
+    mat B = { { std::cos(x(2)), -std::sin(x(2)), 0.0 },
+              { std::sin(x(2)), std::cos(x(2)), 0.0 },
+              { 0.0, 0.0, 1.0 } };
+    return B;
+  }
+
+  unsigned int action_space;  // control space dimension
+  unsigned int state_space;   // states space dimension
 };
 
 }  // namespace ergodic_exploration
