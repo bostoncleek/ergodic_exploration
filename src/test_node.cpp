ecbb752f2c1bf7631a1c29e5478149670c282da2
bostoncleek <bocl8874@colorado.edu>
2020-11-09 20:57:02 -0600

    test collision detection

--- src/test_node.cpp
+++ src/test_node.cpp
@@ -19,6 +19,7 @@
 #include <ergodic_exploration/numerics.hpp>
 #include <ergodic_exploration/ergodic_control.hpp>
 #include <ergodic_exploration/mapping.hpp>
+#include <ergodic_exploration/dynamic_window.hpp>
 
 // #include <armadillo>
 
@@ -38,24 +39,86 @@ int main(int argc, char** argv)
   // spinner.start();
 
   const auto xmin = 0.0;
-  const auto xmax = 10.0;
+  const auto xmax = 2.0;
   const auto ymin = 0.0;
-  const auto ymax = 10.0;
-  const double resolution = 0.05;
+  const auto ymax = 3.0;
+  const double resolution = 0.5;
   const unsigned int xsize = ergodic_exploration::axis_length(xmin, xmax, resolution);
   const unsigned int ysize = ergodic_exploration::axis_length(ymin, ymax, resolution);
   std::vector<int8_t> map_data(xsize * ysize, 50);
 
-  ergodic_exploration::GridMap grid_map(xmin, xmax, ymin, ymax, resolution, map_data);
+  map_data.at(13) = 100;
+
+  ergodic_exploration::GridMap grid(xmin, xmax, ymin, ymax, resolution, map_data);
+
+  double boundary_radius = 1.0;
+  double search_radius = 3.0;
+  double obstacle_threshold = 0.5;
+  double occupied_threshold = 0.9;
+  ergodic_exploration::Collision collision(boundary_radius, search_radius,
+                                           obstacle_threshold, occupied_threshold);
+
+  ergodic_exploration::Omni omni;
+
+  double dt = 0.1;
+  double horizon = 0.5;
+  double acc_lim_x = 1.0;
+  double acc_lim_y = 1.0;
+  double acc_lim_th = 2.0;
+  double max_vel_trans = 1.0;
+  double min_vel_trans = -1.0;
+  double max_vel_x = 1.0;
+  double min_vel_x = -1.0;
+  double max_vel_y = 1.0;
+  double min_vel_y = -1.0;
+  double max_rot_vel = 1.0;
+  double min_rot_vel = -1.0;
+  unsigned int vx_samples = 5;
+  unsigned int vy_samples = 5;
+  unsigned int vth_samples = 10;
+
+  ergodic_exploration::DynamicWindow dwa(omni, dt, horizon, acc_lim_x, acc_lim_y,
+                                         acc_lim_th, max_vel_trans, min_vel_trans,
+                                         max_vel_x, min_vel_x, max_vel_y, min_vel_y,
+                                         max_rot_vel, min_rot_vel, vx_samples, vy_samples,
+                                         vth_samples);
+
+  vec x = { 0.0, 0.0, PI / 2.0 };
+  vec vb = { 0.0, 0.0, 0.0 };
+  vec goal = { 7.0, 5.0, PI / 2.0 };
+
+  //////////////////////////////////////////////////////////////////////////////
+  clock_t c_start = clock();
+  auto t_start = std::chrono::high_resolution_clock::now();
+
+  // dwa.control(collision, grid, x, goal, vb);
+
+  double dmin = 0.0;
+  if (collision.minDistance(dmin, grid, x) == ergodic_exploration::CollisionMsg::crash)
+  {
+    std::cout << "collision" << std::endl;
+  }
+  std::cout << "dis: " << dmin << std::endl;
+
+  vec disp;
+  if (collision.minDirection(disp, grid, x) == ergodic_exploration::CollisionMsg::obstacle)
+  {
+    disp.print();
+  }
+
+  clock_t c_end = clock();
+  auto t_end = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Clock frequency (Hz): "
+            << 1.0 / (static_cast<double>((c_end - c_start)) /
+                      static_cast<double>(CLOCKS_PER_SEC))
+            << " Wall frequency (Hz): "
+            << 1.0 / (std::chrono::duration<double, std::milli>(t_end - t_start).count() /
+                      1000.0)
+            << std::endl;
+  //////////////////////////////////////////////////////////////////////////////
 
   // // std::cout <<  << std::endl;
-  //
-  // double boundary_radius = 1.0;
-  // double search_radius = 3.0;
-  // double obstacle_threshold = 0.5;
-  // double occupied_threshold = 0.9;
-  // ergodic_exploration::Collision collision_detector(
-  //     boundary_radius, search_radius, obstacle_threshold, occupied_threshold);
 
   // std::cout << "xsize: " << xsize << std::endl;
   // std::cout << "ysize: " << ysize << std::endl;
@@ -132,83 +195,53 @@ int main(int argc, char** argv)
   // std::cout << "entropy: " << ergodic_exploration::entropy(-1.0/100) << std::endl;
   // std::cout << "entropy: " << ergodic_exploration::entropy(0.5) << std::endl;
 
-  // const auto horizon = 0.5;
-  // const auto dt = 0.1;
-  // const auto N = static_cast<unsigned int>(horizon / std::abs(dt));
+  // unsigned int num_pts = 1080;
+  // float angle_min = 0.0;
+  // float angle_max = 2.0 * PI;
+  // float angle_increment = (angle_max - angle_min) / static_cast<float>(num_pts);
+  // std::vector<float> ranges(num_pts, 2.0);
   //
-  // arma::mat ut(4, N);
-  // ut.row(0).fill(1.0);
-  // ut.row(1).fill(1.0);
-  // ut.row(2).fill(1.0);
-  // ut.row(3).fill(1.0);
+  // // float angle = angle_min;
+  // // for (unsigned int i = 0; i < num_pts; i++)
+  // // {
+  // //   ranges.at(i) += angle_min;
+  // //   angle_min += 0.1;
+  // // }
   //
-  // // arma::vec x0 = { 0.0, 0.0, 0.0 };
-  // arma::vec x0 = { 0.1, 0.0, 0.0 };
-  // ergodic_exploration::RungeKutta rk4(dt);
+  // sensor_msgs::LaserScan scan_msg;
+  // scan_msg.angle_min = angle_min;
+  // scan_msg.angle_max = angle_max;
+  // scan_msg.angle_increment = angle_increment;
+  // scan_msg.ranges = ranges;
   //
-  // // arma::vec x1 = rk4.step(mecanum, x0, ut.col(0));
-  // // x1.print("x1:");
+  // auto scan_ptr = boost::make_shared<const sensor_msgs::LaserScan>(scan_msg);
   //
-  // arma::mat xt = rk4.solve(mecanum, x0, ut, horizon);
-  // xt.print("xt:");
-
-  // collision_detector.obstacleCells(grid_map, 100, 100);
-  // ergodic_exploration::CollisionMap collision_map = collision_detector.collisionMap();
+  // // vec x = { 5.0, 5.0, PI / 2.0 };
+  // vec x = { 5.0, 5.0, 0.0 };
+  // mat Tbs = ergodic_exploration::transform2d(-PI / 2.0);
+  // // Tbs.print();
   //
-  // std::cout << "size: " << collision_map.size() << std::endl;
+  // ergodic_exploration::OccupancyMapper mapper(Tbs);
   //
-  // for (const auto item : collision_map)
-  // {
-  //   map_data.at(item.first) = 0;
-  // }
-
-  unsigned int num_pts = 1080;
-  float angle_min = 0.0;
-  float angle_max = 2.0 * PI;
-  float angle_increment = (angle_max - angle_min) / static_cast<float>(num_pts);
-  std::vector<float> ranges(num_pts, 2.0);
-
-  // float angle = angle_min;
-  // for (unsigned int i = 0; i < num_pts; i++)
-  // {
-  //   ranges.at(i) += angle_min;
-  //   angle_min += 0.1;
-  // }
-
-  sensor_msgs::LaserScan scan_msg;
-  scan_msg.angle_min = angle_min;
-  scan_msg.angle_max = angle_max;
-  scan_msg.angle_increment = angle_increment;
-  scan_msg.ranges = ranges;
-
-  auto scan_ptr = boost::make_shared<const sensor_msgs::LaserScan>(scan_msg);
-
-  // vec x = { 5.0, 5.0, PI / 2.0 };
-  vec x = { 5.0, 5.0, 0.0 };
-  mat Tbs = ergodic_exploration::transform2d(-PI / 2.0);
-  // Tbs.print();
-
-  ergodic_exploration::OccupancyMapper mapper(Tbs);
-
-  auto c_start = std::clock();
-  auto t_start = std::chrono::high_resolution_clock::now();
-
-  mapper.updateMap(grid_map, scan_ptr, x);
+  // auto c_start = std::clock();
+  // auto t_start = std::chrono::high_resolution_clock::now();
+  //
   // mapper.updateMap(grid_map, scan_ptr, x);
-  // mapper.updateMap(grid_map, scan_ptr, x);
-  // mapper.updateMap(grid_map, scan_ptr, x);
-
-  auto c_end = std::clock();
-  auto t_end = std::chrono::high_resolution_clock::now();
-
-  std::cout << "Clock frequency (Hz): "
-            << 1.0 / (static_cast<double>((c_end - c_start)) /
-                      static_cast<double>(CLOCKS_PER_SEC))
-            << " Wall frequency (Hz): "
-            << 1.0 / (std::chrono::duration<double, std::milli>(t_end - t_start).count() /
-                      1000.0)
-            << std::endl;
-
+  // // mapper.updateMap(grid_map, scan_ptr, x);
+  // // mapper.updateMap(grid_map, scan_ptr, x);
+  // // mapper.updateMap(grid_map, scan_ptr, x);
+  //
+  // auto c_end = std::clock();
+  // auto t_end = std::chrono::high_resolution_clock::now();
+  //
+  // std::cout << "Clock frequency (Hz): "
+  //           << 1.0 / (static_cast<double>((c_end - c_start)) /
+  //                     static_cast<double>(CLOCKS_PER_SEC))
+  //           << " Wall frequency (Hz): "
+  //           << 1.0 / (std::chrono::duration<double, std::milli>(t_end - t_start).count() /
+  //                     1000.0)
+  //           << std::endl;
+  //
   nav_msgs::OccupancyGrid map;
   map.header.frame_id = "map";
   map.info.resolution = resolution;
@@ -217,7 +250,7 @@ int main(int argc, char** argv)
   map.info.width = xsize;
   map.info.height = ysize;
   // map.data = map_data;
-  map.data = grid_map.gridData();
+  map.data = grid.gridData();
 
   ros::Publisher map_pub = nh.advertise<nav_msgs::OccupancyGrid>("map", 1);
 
