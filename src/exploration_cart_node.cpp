6e581ebcf5c7bdfb8d6a657eb3a3a9170175c00d
bostoncleek <bocl8874@colorado.edu>
2020-12-04 18:16:06 -0600

    set y-vel and y-accel to 0

--- src/exploration_cart_node.cpp
+++ src/exploration_cart_node.cpp
@@ -5,17 +5,16 @@
  * @brief Ergodic exploration using a differential drive cart
 
  PARAMETERS:
+    map_frame_id - map frame id
+    base_frame_id - base link frame id
     frequency - control loop frequency (Hz)
     val_dt - control validation time step for collision detection (s)
     val_horizon - control validation horizon for collision detection (s)
     max_vel_x - max x velcocity (m/s)
-    max_vel_y -  max y velcocity (m/s)
     max_rot_vel - max ratotional velcocity (m/s)
     min_vel_x - min x velcocity (m/s)
-    min_vel_y - min y velcocity (m/s)
     min_rot_vel - min ratotional velcocity (m/s)
     acc_lim_x - x acceleration limit (m/s^2)
-    acc_lim_y - y acceleration limit (m/s^2)
     acc_lim_th - rotational acceleration limit (m/s^2)
     boundary_radius - bounding radius around robot (m)
     search_radius - max search radius for collision detection (m)
@@ -28,12 +27,11 @@
     num_basis - number of basis functions
     buffer_size - total number of past states stored
     batch_size - number of past states randomly sampled in each control loop
-    control_weights - weights on twist [vx vy w]
+    control_weights - weights on twist [vx w]
     dwa_dt - time step used in integration (s)
     dwa_horizon - control horizon (s)
     acc_dt - time step the acceleration limits are applied (s)
     vx_samples - number of x velcocity samples
-    vy_samples - number of y velcocity samples
     vth_samples - number of rotational velcocity samples
     means - target x and y means (m)
     sigmas - target x and y standard deviations (m)
@@ -94,19 +92,16 @@ int main(int argc, char** argv)
   const double val_horizon = pnh.param("val_horizon", 0.5);
 
   const double max_vel_x = pnh.param("max_vel_x", 1.0);
-  const double max_vel_y = pnh.param("max_vel_y", 1.0);
   const double max_rot_vel = pnh.param("max_rot_vel", 1.0);
 
   const double min_vel_x = pnh.param("min_vel_x", -1.0);
-  const double min_vel_y = pnh.param("min_vel_y", -1.0);
   const double min_rot_vel = pnh.param("min_rot_vel", -1.0);
 
   const double acc_lim_x = pnh.param("acc_lim_x", 1.0);
-  const double acc_lim_y = pnh.param("acc_lim_y", 1.0);
   const double acc_lim_th = pnh.param("acc_lim_th", 1.0);
 
-  const vec umin = { min_vel_x, min_vel_y, min_rot_vel };
-  const vec umax = { max_vel_x, max_vel_y, max_rot_vel };
+  const vec umin = { min_vel_x, 0.0, min_rot_vel };
+  const vec umax = { max_vel_x, 0.0, max_rot_vel };
 
   // collision
   const double boundary_radius = pnh.param("boundary_radius", 0.7);
@@ -123,20 +118,19 @@ int main(int argc, char** argv)
   const unsigned int buffer_size = pnh.param("buffer_size", 1e6);
   const unsigned int batch_size = pnh.param("batch_size", 100);
 
-  std::vector<double> control_weights = { 1.0, 1.0, 1.0 };
+  std::vector<double> control_weights = { 1.0, 1.0 };
   pnh.getParam("control_weights", control_weights);
 
   mat R(3, 3, arma::fill::zeros);
   R(0, 0) = control_weights.at(0);
-  R(1, 1) = control_weights.at(1);
-  R(2, 2) = control_weights.at(2);
+  R(1, 1) = 0.0;
+  R(2, 2) = control_weights.at(1);
 
   // dwa
   const double dwa_dt = pnh.param("dwa_dt", 0.1);
   const double dwa_horizon = pnh.param("dwa_horizon", 1.0);
   const double acc_dt = pnh.param("acc_dt", 0.2);
   const unsigned int vx_samples = pnh.param("vx_samples", 3);
-  const unsigned int vy_samples = pnh.param("vy_samples", 8);
   const unsigned int vth_samples = pnh.param("vth_samples", 5);
 
   if (dwa_horizon > ec_horizon)
@@ -169,9 +163,9 @@ int main(int argc, char** argv)
                                  expl_weight, num_basis, buffer_size, batch_size, R, umin,
                                  umax);
 
-  DynamicWindow dwa(collision, dwa_dt, dwa_horizon, acc_dt, acc_lim_x, acc_lim_y,
-                    acc_lim_th, max_vel_x, min_vel_x, max_vel_y, min_vel_y, max_rot_vel,
-                    min_rot_vel, vx_samples, vy_samples, vth_samples);
+  DynamicWindow dwa(collision, dwa_dt, dwa_horizon, acc_dt, acc_lim_x, 0.0,
+                    acc_lim_th, max_vel_x, min_vel_x, 0.0, 0.0, max_rot_vel,
+                    min_rot_vel, vx_samples, 0.0, vth_samples);
 
   ergodic_exploration::Exploration<SimpleCart> exploration(nh, ergodic_control, collision,
                                                            dwa);
